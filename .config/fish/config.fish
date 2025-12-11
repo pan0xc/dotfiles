b6eb60552f815b7482ed03862321e311cf916249
hooke <hellohooke@foxmail.com>
2025-12-12 04:58:12 +0800

    [fish] add env val of xdg go and rust

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,3 +16,17 @@ function fish_greeting
     echo $greet
 end
 
+# XDG Base Directory
+set -x XDG_CONFIG_HOME $HOME/.config
+set -x XDG_CACHE_HOME $HOME/.cache
+set -x XDG_DATA_HOME $HOME/.local/share
+set -x XDG_CACHE_HOME $HOME/.cache
+set -x XDG_STATE_HOME $HOME/.local/state
+
+# Go
+set -x GOPATH $XDG_DATA_HOME/go
+set -x PATH $GOPATH/bin $PATH
+
+# Rust
+set -x CARGO_HOME $XDG_DATA_HOME/cargo
+set -x PATH $CARGO_HOME/bin $PATH
