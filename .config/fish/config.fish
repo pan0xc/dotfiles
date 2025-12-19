a667b1f38a3668049fc55c24ae8adc4b7c0bfdd6
hooke <hellohooke@foxmail.com>
2025-12-20 00:31:47 +0800

    [fish] fix libc export

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,14 +21,17 @@ set -x PATH $CARGO_HOME/bin $PATH
 set -x LIBC_HOME $XDG_DATA_HOME/libc
 for d in $LIBC_HOME/*
     if test -d $d/include
-        set -x C_INCLUDE_PATH $d/include
-        set -x CPLUS_INCLUDE_PATH $d/include
+        set -xa C_INCLUDE_PATH $d/include
+        set -xa CPLUS_INCLUDE_PATH $d/include
     end
-end
-for d in $LIBC_HOME/*
     if test -d $d/lib64
-        set -x LIBRARY_PATH $d/lib64
-        set -x LD_LIBRARY_PATH $d/lib64
-        set -x PKG_CONFIG_PATH $d/lib64/pkgconfig
+        set -xa LIBRARY_PATH $d/lib64
+        set -xa LD_LIBRARY_PATH $d/lib64
+        set -xa PKG_CONFIG_PATH $d/lib64/pkgconfig
+    end
+    if test -d $d/lib
+        set -xa LIBRARY_PATH $d/lib
+        set -xa LD_LIBRARY_PATH $d/lib
+        set -xa PKG_CONFIG_PATH $d/lib/pkgconfig
     end
 end
