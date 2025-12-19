212d8806f72c3aaa3b71883a390437a64be57873
hooke <hellohooke@foxmail.com>
2025-12-19 10:38:14 +0000

    [fish] add libc and export C/C++ include

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,3 +16,19 @@ set -x PATH $GOPATH/bin $PATH
 # Rust
 set -x CARGO_HOME $XDG_DATA_HOME/cargo
 set -x PATH $CARGO_HOME/bin $PATH
+
+# C/C++
+set -x LIBC_HOME $XDG_DATA_HOME/libc
+for d in $LIBC_HOME/*
+    if test -d $d/include
+        set -x C_INCLUDE_PATH $d/include
+        set -x CPLUS_INCLUDE_PATH $d/include
+    end
+end
+for d in $LIBC_HOME/*
+    if test -d $d/lib64
+        set -x LIBRARY_PATH $d/lib64
+        set -x LD_LIBRARY_PATH $d/lib64
+        set -x PKG_CONFIG_PATH $d/lib64/pkgconfig
+    end
+end
