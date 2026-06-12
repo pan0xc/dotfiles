525cdb4984216bd35bc3a41b414764ed9b02de9a
Chen Pan <pan0xc@foxmail.com>
2026-06-12 16:06:04 +0800

    [direnv]: update

--- .config/fish/conf.d/direnv.fish
+++ .config/fish/conf.d/direnv.fish
@@ -1,1 +1,5 @@
 direnv hook fish | source
+
+set -x direnv_fish_mode eval_on_arrow    # trigger direnv at prompt, and on every arrow-based directory change (default)
+set -x direnv_fish_mode eval_after_arrow # trigger direnv at prompt, and only after arrow-based directory changes before executing command
+set -x direnv_fish_mode disable_arrow    # trigger direnv at prompt only, this is similar functionality to the original behavior
